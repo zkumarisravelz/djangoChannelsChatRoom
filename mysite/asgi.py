217e9d1697363afbd570ee97b8209ee91759bd94
Naga Vignesh Kumar T <Naga.Kumar@bond-pro.com>
2021-09-22 18:46:49 +0530

    First Commit

---
+++ mysite/asgi.py
@@ -0,0 +1,29 @@
+"""
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = ProtocolTypeRouter({
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+
+                routing.websocket_urlpatterns
+            )
+        ),
+    })
+
